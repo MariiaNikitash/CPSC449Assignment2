234c4e9265f04090b3f29e94293e86c8e2413514
Mariia Nikitash <nikitash.mariia2004@csu.fullerton.edu>
2026-04-23 22:40:52 -0700

    added screenshots of api

--- README.md
+++ README.md
@@ -3,6 +3,10 @@ Mariia Nikitash <br>
 CWID: 885919944
 
 ### Register the ADMIN user (200 OK response)
+![1](screenshots/1.png)
 ### Login as ADMIN and token visible in response
+![2](screenshots/2.png)
 ### DELETE request as ADMIN — 200 OK with success message
+![3](screenshots/3.png)
 ### DELETE request as USER — 403 Forbidden response
+![4](screenshots/4.png)
--- bookstore_mongodb/src/main/java/com/example/bookstore_mongodb/config/SecurityConfig.java
+++ bookstore_mongodb/src/main/java/com/example/bookstore_mongodb/config/SecurityConfig.java
@@ -19,6 +19,9 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class SecurityConfig {
 
     @Autowired
+    private AccessDeniedSecHandler accessDeniedHandler;
+
+    @Autowired
     private JwtFilter jwtFilter;
 
     @Bean
@@ -29,8 +32,9 @@ public class SecurityConfig {
                         .requestMatchers(org.springframework.http.HttpMethod.GET, "/api/books/**").permitAll()
                         .requestMatchers(org.springframework.http.HttpMethod.POST, "/api/books/**").hasRole("USER")
                         .requestMatchers(org.springframework.http.HttpMethod.DELETE, "/api/books/**").hasRole("ADMIN")
-                        .anyRequest().authenticated()
-                )
+                        .anyRequest().authenticated())
+                .exceptionHandling(ex -> ex
+                        .accessDeniedHandler(accessDeniedHandler))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 
